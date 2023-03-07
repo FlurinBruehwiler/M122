617be997a19e4c8c9ca4a8f6044e2715f2382d12
FlurinBruehwilerCMI <69467225+FlurinBruehwilerCMI@users.noreply.github.com>
2023-03-07 16:57:38 +0100

    added xml file generation and ftp upload

--- Handwerkerrechnung/Program.cs
+++ Handwerkerrechnung/Program.cs
@@ -1,23 +1,22 @@
 ﻿using System.Globalization;
 using System.Net;
-using System.Runtime.InteropServices.JavaScript;
 using FluentFTP;
 using FluentFTP.Helpers;
 
-// using var client = new FtpClient
-// {
-//     Host = "ftp.haraldmueller.ch",
-//     Credentials = new NetworkCredential("schueler", "studentenpasswort")
-// };
-//
-// client.Connect();
-//
-// var stream = new MemoryStream();
-// client.DownloadStream(stream, "/M122-AP20b/Bruehwiler");
-// var reader = new StreamReader(stream);
-// var text = reader.ReadToEnd();
+using var haraldClient = new FtpClient
+{
+    Host = "ftp.haraldmueller.ch",
+    Credentials = new NetworkCredential("schueler", "studentenpasswort")
+};
 
-var text = File.ReadAllText("Input.data");
+haraldClient.Connect();
+
+var stream = new MemoryStream();
+haraldClient.DownloadStream(stream, "/M122-AP20b/Bruehwiler");
+var reader = new StreamReader(stream);
+var text = reader.ReadToEnd();
+
+// var text = File.ReadAllText("Input.data");
 
 var fileContnet = text.Split("\n").Select(x => x.Split(";")).ToArray();
 
@@ -31,6 +30,7 @@ var auftragsnummer = header[1].RemovePrefix("Auftrag_"); //A003
 
 var datum = header[3]; //21.07.2023
 
+var herkunft_id = herkunft[1];
 var herkunft_name = herkunft[3]; //Adam Adler
 var herkunft_adresse1 = herkunft[4]; //Bahnhofstrasse 1
 var herkunft_adresse2 = herkunft[5].Trim(); //8000 Zuerich
@@ -38,6 +38,7 @@ var herkunft_adresse2 = herkunft[5].Trim(); //8000 Zuerich
 var kundennummer = herkunft[2]; //K821
 var rechnungsname = herkunft[6]; //CHE-111.222.333 MWST
 
+var endkund_id = endkunde[1];
 var endkunde_name = endkunde[2]; //Autoleasing AG
 var endkunde_adresse1 = endkunde[3]; //Gewerbestrasse 100
 var endkunde_adresse2 = endkunde[4].Trim(); //5000 Aarau
@@ -173,3 +174,171 @@ File.WriteAllText(txtFileName, txt);
 //Generate Rechnung als XML
 
 var xmlFileName = "[Kundennummer]_[Rechnungsnummer]_invoice.xml";
+
+var xml = $"""
+<XML-FSCM-INVOICE-2003A>
+    <INTERCHANGE>
+        <IC-SENDER>
+            <Pid>{herkunft_id}</Pid>
+        </IC-SENDER>
+        <IC-RECEIVER>
+            <Pid>{endkund_id}</Pid>
+        </IC-RECEIVER>
+        <IR-Ref />
+    </INTERCHANGE>
+    <INVOICE>
+        <HEADER>
+            <FUNCTION-FLAGS>
+                <Confirmation-Flag />
+                <Canellation-Flag />
+            </FUNCTION-FLAGS>
+            <MESSAGE-REFERENCE>
+                <REFERENCE-DATE>
+                    <Reference-No>202307314522001</Reference-No>
+                    <Date>{startDate:yyyyMMdd}</Date>
+                </REFERENCE-DATE>
+            </MESSAGE-REFERENCE>
+            <PRINT-DATE>
+                <Date>{startDate:yyyyMMdd}</Date>
+            </PRINT-DATE>
+            <REFERENCE>
+                <INVOICE-REFERENCE>
+                    <REFERENCE-DATE>
+                        <Reference-No>{rechnungsnummer}</Reference-No>
+                        <Date>{startDate:yyyyMMdd}</Date>
+                    </REFERENCE-DATE>
+                </INVOICE-REFERENCE>
+                <ORDER>
+                    <REFERENCE-DATE>
+                        <Reference-No>{auftragsnummer}</Reference-No>
+                        <Date>{startDate:yyyyMMdd}</Date>
+                    </REFERENCE-DATE>
+                </ORDER>
+                <REMINDER Which="MAH">
+                    <REFERENCE-DATE>
+                        <Reference-No></Reference-No>
+                        <Date></Date>
+                    </REFERENCE-DATE>
+                </REMINDER>
+                <OTHER-REFERENCE Type="ADE">
+                    <REFERENCE-DATE>
+                        <Reference-No>202307164522001</Reference-No>
+                        <Date>{startDate:yyyyMMdd}</Date>
+                    </REFERENCE-DATE>
+                </OTHER-REFERENCE>
+            </REFERENCE>
+            <BILLER>
+                <Tax-No>{rechnungsname}</Tax-No>
+                <Doc-Reference Type="ESR-ALT "></Doc-Reference>
+                <PARTY-ID>
+                    <Pid>{herkunft_id}</Pid>
+                </PARTY-ID>
+                <NAME-ADDRESS Format="COM">
+                    <NAME>
+                        <Line-35>{herkunft_name}</Line-35>
+                        <Line-35>{herkunft_adresse1}</Line-35>
+                        <Line-35>{herkunft_adresse2}</Line-35>
+                        <Line-35></Line-35>
+                        <Line-35></Line-35>
+                    </NAME>
+                    <STREET>
+                        <Line-35></Line-35>
+                        <Line-35></Line-35>
+                        <Line-35></Line-35>
+                    </STREET>
+                    <City></City>
+                    <State></State>
+                    <Zip></Zip>
+                    <Country></Country>
+                </NAME-ADDRESS>
+                <BANK-INFO>
+                    <Acct-No></Acct-No>
+                    <Acct-Name></Acct-Name>
+                    <BankId Type="BCNr-nat" Country="CH">001996</BankId>
+                </BANK-INFO>
+            </BILLER>
+            <PAYER>
+                <PARTY-ID>
+                    <Pid>{endkund_id}</Pid>
+                </PARTY-ID>
+                <NAME-ADDRESS Format="COM">
+                    <NAME>
+                        <Line-35>{endkunde_name}</Line-35>
+                        <Line-35>{endkunde_adresse1}</Line-35>
+                        <Line-35>{endkunde_adresse2}</Line-35>
+                        <Line-35></Line-35>
+                        <Line-35></Line-35>
+                    </NAME>
+                    <STREET>
+                        <Line-35></Line-35>
+                        <Line-35></Line-35>
+                        <Line-35></Line-35>
+                    </STREET>
+                    <City></City>
+                    <State></State>
+                    <Zip></Zip>
+                    <Country></Country>
+                </NAME-ADDRESS>
+            </PAYER>
+        </HEADER>
+        <LINE-ITEM />
+        <SUMMARY>
+            <INVOICE-AMOUNT>
+                <Amount>0000135000</Amount>
+            </INVOICE-AMOUNT>
+            <VAT-AMOUNT>
+                <Amount></Amount>
+            </VAT-AMOUNT>
+            <DEPOSIT-AMOUNT>
+                <Amount></Amount>
+                <REFERENCE-DATE>
+                    <Reference-No></Reference-No>
+                    <Date></Date>
+                </REFERENCE-DATE>
+            </DEPOSIT-AMOUNT>
+            <EXTENDED-AMOUNT Type="79">
+                <Amount></Amount>
+            </EXTENDED-AMOUNT>
+            <TAX>
+                <TAX-BASIS>
+                    <Amount></Amount>
+                </TAX-BASIS>
+                <Rate Categorie="S">0</Rate>
+                <Amount></Amount>
+            </TAX>
+            <PAYMENT-TERMS>
+                <BASIC Payment-Type="ESR" Terms-Type="1">
+                    <TERMS>
+                        <Payment-Period Type="M" On-Or-After="1" Reference-Day="31">30</Payment-Period>
+                        <Date>20230830</Date>
+                    </TERMS>
+                </BASIC>
+                <DISCOUNT Terms-Type="22">
+                    <Discount-Percentage>0.0</Discount-Percentage>
+                    <TERMS>
+                        <Payment-Period Type="M" On-Or-After="1" Reference-Day="31"></Payment-Period>
+                        <Date></Date>
+                    </TERMS>
+                    <Back-Pack-Container Encode="Base64"> </Back-Pack-Container>
+                </DISCOUNT>
+            </PAYMENT-TERMS>
+        </SUMMARY>
+    </INVOICE>
+</XML-FSCM-INVOICE-2003A>
+""";
+
+using var conditoreiClient = new FtpClient
+{
+    Host = "ftp.coinditorei.com",
+    Credentials = new NetworkCredential("zahlungssystem", "Berufsschule8005")
+};
+
+conditoreiClient.Connect();
+
+var textStream = new MemoryStream();
+new StreamWriter(textStream).Write(text);
+haraldClient.UploadStream(textStream, $"in/AP20bBruehwiler/{txtFileName}");
+
+var xmlStream = new MemoryStream();
+new StreamWriter(xmlStream).Write(xml);
+haraldClient.UploadStream(xmlStream, $"in/AP20bBruehwiler/{xmlFileName}");
